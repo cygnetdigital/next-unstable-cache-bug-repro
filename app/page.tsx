0d679918bd5dfcb2cc4e0bd3b2dd49f2bc18545d
Nick Jackson <nick@cygnet.io>
2025-02-20 16:28:49 +0000

    repro

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,58 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { unstable_cache } from "next/cache";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string };
+}) {
+  const data = await fetchData(searchParams?.key as string);
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
+}
+
+interface IpData {
+  as: string;
+  city: string;
+  country: string;
+  countryCode: string;
+  isp: string;
+  lat: number;
+  lon: number;
+  org: string;
+  query: string;
+  region: string;
+  regionName: string;
+  status: string;
+  timezone: string;
+  zip: string;
+}
+
+interface Data extends IpData {
+  utcTime: string;
+  key: string;
+}
+
+// fetchData will go fetch some actual data from an API, but in theory it
+// should do it only once per provided key.
+//
+// In practice the callback (fcb) will be invoked multiple times for the same
+// key, when additional non english characters are present in completely unrelated
+// query parameters.
+async function fetchData(key: string): Promise<Data> {
+  const fcb = async () => {
+    const res = await fetch("http://ip-api.com/json/", { cache: "no-cache" });
+    const data = await res.json();
+    return {
+      ...data,
+      utcTime: new Date().toUTCString(),
+      key,
+    };
+  };
+
+  const uc = unstable_cache(fcb, [key], {});
+  return uc();
 }
